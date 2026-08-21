2bc63616411e32e6415a8053929079a82b1d1790
CHANDRU-1U25CS015 <chandru94756@gmail.com>
2026-08-21 11:31:23 +0530

    Add NOT NULL constraint to HOD column in Department table

--- starter.sql
+++ starter.sql
@@ -1,7 +1,6 @@
 CREATE DATABASE CollegeDB;
 USE CollegeDB;
-CREATE TABLE Department(Department ID integer(10) primary key,DepartmentName varchar(20),HOD varchar(20)
-);
+CREATE TABLE Department(Department ID integer(10) primary key,DepartmentName varchar(20),HOD varchar(20)Not null);
 DESC Department;
 INSERT Department values("101","Chandru","Jayandhu");
 SELECT*FROM Department;
